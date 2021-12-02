31cc82f3a562efd9c86b4b50a1d308c8f93ff72b
mostafa170 <most@fci_1995>
2021-12-02 13:56:42 +0200

    add more than page

---
+++ app/src/main/java/com/devartlab/ui/main/ui/moreDetailsAds/MoreDetailsAdsActivity.java
@@ -0,0 +1,16 @@
+package com.devartlab.ui.main.ui.moreDetailsAds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devartlab.R;
+
+public class MoreDetailsAdsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_details_ads);
+    }
+}
