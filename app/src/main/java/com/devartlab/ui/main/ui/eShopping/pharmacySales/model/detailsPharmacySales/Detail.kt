fe24518e394a9edc26f5b9bcab62664f78e1ed63
mostafa170 <mostafakamelkamel@gmail.com>
2022-01-30 21:43:26 +0200

    edit model DetailsPharmacySalesResponse

--- app/src/main/java/com/devartlab/ui/main/ui/eShopping/pharmacySales/model/detailsPharmacySales/Detail.kt
+++ app/src/main/java/com/devartlab/ui/main/ui/eShopping/pharmacySales/model/detailsPharmacySales/Detail.kt
@@ -1,17 +1,7 @@
 package com.devartlab.ui.main.ui.eShopping.pharmacySales.model.detailsPharmacySales
 
 data class Detail(
-    val color: String,
-    val dp: String,
     val item: Item,
-    val keys: String,
-    val license: String,
     val price: Double,
-    val qty: Double,
-    val size: String,
-    val size_key: Double,
-    val size_price: String,
-    val size_qty: String,
-    val stock: Double,
-    val values: String
+    val qty: Double
 )
--- app/src/main/java/com/devartlab/ui/main/ui/eShopping/pharmacySales/model/detailsPharmacySales/Subcat.kt
+++
@@ -1,7 +0,0 @@
-package com.devartlab.ui.main.ui.eShopping.pharmacySales.model.detailsPharmacySales
-
-data class Subcat(
-    val id: Int,
-    val name: String,
-    val name_ar: String
-)
