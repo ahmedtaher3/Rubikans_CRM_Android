19a2f36b7ab2a33750c7cfc0e4fc9bc780901c95
mostafa170 <mostafakamelkamel@gmail.com>
2022-02-21 10:26:33 +0200

    edit handbook api

--- app/src/main/java/com/devartlab/ui/main/ui/devartlink/handBook/SectionsAdapter.java
+++ app/src/main/java/com/devartlab/ui/main/ui/devartlink/handBook/SectionsAdapter.java
@@ -1,29 +1,16 @@
 package com.devartlab.ui.main.ui.devartlink.handBook;
 
-import static com.mapbox.mapboxsdk.Mapbox.getApplicationContext;
-
 import android.content.Context;
-import android.graphics.drawable.Drawable;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
-import androidx.core.text.HtmlCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
 import com.devartlab.R;
-import com.devartlab.databinding.HandBookItemBinding;
 import com.devartlab.databinding.SectionsItemBinding;
 import com.devartlab.ui.main.ui.devartlink.handBook.model.Section;
-
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,8 +38,6 @@ public class SectionsAdapter extends RecyclerView.Adapter<SectionsAdapter.ViewHo
         Glide.with(context)
                 .load("https://devartlink.devartlab.com/assets/images/" + dataItem.getImage())
                     .fitCenter().into(viewHolder.binding.imgSubject);
-        String b = dataItem.getDescription().replace("\\\"", "\"");
-        writeToFile(b, context);
         viewHolder.binding.wbSections.loadDataWithBaseURL(
                 null, dataItem.getDescription(), "text/html", "utf-8", null);
     }
@@ -92,15 +77,4 @@ public class SectionsAdapter extends RecyclerView.Adapter<SectionsAdapter.ViewHo
         this.dataItems = newList;
         notifyDataSetChanged(); // refresh
     }
-    private void writeToFile(String data, Context context) {
-        try {
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("form.txt", Context.MODE_PRIVATE));
-            outputStreamWriter.write(data);
-            Log.e("Exception", "File write success: " + data);
-            outputStreamWriter.close();
-        } catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
-        }
-    }
-
 }
